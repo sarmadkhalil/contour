b2b7c3b147728e27692a379f65adb0fff479bff8
sarmadkhalil <41991331+sarmadkhalil@users.noreply.github.com>
2021-04-13 11:41:05 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Route::get('getuser', [App\Http\Controllers\UserController::class, 'getUser'])->name('users');
-// Route::get('/users', \App\Http\Livewire\Users\Users::class)->name('users');
-Route::get('users/index', [App\Http\Controllers\UserController::class, 'indexPage'])->name('users');
+Route::get('/', [App\Http\Controllers\UserController::class, 'indexPage'])->name('users');
 Route::post('users/charts', [App\Http\Controllers\UserController::class, 'getDataForCharts'])->name('charts');
 
